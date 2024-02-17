5f4cee01432781354818ed76e4ad0cde41c13f39
firekilin <wl00161839@gmail.com>
2024-02-17 12:00:15 +0800

    測試

--- app/main.js
+++ app/main.js
@@ -29,9 +29,11 @@ router.post ('/login', async(req, res) => {
   try {
     const response = await axios.get ('https://api.line.me/v2/profile',
       { headers: { 'Authorization': `Bearer ${req.body.accessToken}` } });
-
+    console.log (response.data);
     res.json (utils.response (response.data));
   } catch (error) {
+    console.log (error);
+
     res.send (utils.response ('登入錯誤', '0001'));
   }
 
