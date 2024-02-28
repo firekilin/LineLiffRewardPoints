6ad4a25db8fd8a580fb64013b60db60f69df8469
firekilin <wl00161839@gmail.com>
2024-02-29 00:33:53 +0800

    更新測試

--- test2.js
+++ test2.js
@@ -2,13 +2,13 @@ const fs = require ('fs');
 
 
 // 读取 PNG 文件为 Buffer
-const pngBuffer = fs.readFileSync ('1.png');
+const pngBuffer = fs.readFileSync ('000.png');
 
 // 将 Buffer 转换为十六进制字符串
 const hexString = pngBuffer.toString ('hex');
 
 
 // 将十六进制字符串写入文本文件
-fs.writeFileSync ('1.txt', hexString);
+fs.writeFileSync ('000.txt', hexString);
 
-console.log ('PNG 文件已转换为文本文件 output.txt');
+console.log ('PNG 文件已转换为文本文件 000.txt');
--- test4.js
+++ test4.js
@@ -1,3 +1,12 @@
 const crc = require ('crc'); // 引入crc库
 
-console.log (crc.crc32 ('hello').toString (16));
+console.log (crc.crc32 ('hello'));
+
+
+const PLTEBuffer = Buffer.from ('fcTL', 'ascii');
+console.log (PLTEBuffer);
+
+
+
+
+
